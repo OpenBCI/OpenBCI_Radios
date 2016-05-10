97956ae30f57573f98d19387b37b1016666d87d1
AJ Keller <pushtheworldllc@gmail.com>
2016-05-10 07:58:07 -0400

    ADD pass through code

--- examples/32bitRadioPassThru/32bitRadioPassThru.ino
+++ examples/32bitRadioPassThru/32bitRadioPassThru.ino
@@ -1,29 +1,16 @@
-/* 
-
-  OpenBCI Dongle Dummy
-    This code is used to turn the Dongle into a device that will pass FTDI signals
-    It puts pins 0 and 1 into high Z so they won't interfere with the serial transmission signals.
-    
-    Made by Joel Murphy, Summer 2014
-    
-*/
-
-int LED = 2;
-
+/**
+ * OpenBCI Dongle Dummy
+ *    This code is used to turn the Dongle into a device that will pass FTDI signals
+ *    It puts pins 0 and 1 into high Z so they won't interfere with the serial transmission signals.
+ *
+ *    Made by Joel Murphy, Summer 2014
+ *    Refactored by AJ Keller (@pushtheworldllc), Spring 2016
+ */
 void setup() {
-  pinMode(0,OUTPUT_D0H1);  // output is highZ when logic 0, HIGH when logic 1
-  pinMode(1,OUTPUT_D0H1);
-  digitalWrite(0,LOW);
-  digitalWrite(1,LOW);
-  
-  pinMode(LED,OUTPUT);
+  // put your setup code here, to run once:
+  OpenBCI_Radio.begin(OPENBCI_MODE_PASS_THRU);
 }
 
 void loop() {
-  
-  digitalWrite(LED,HIGH);
-  delay(600);
-  digitalWrite(LED,LOW);
-  delay(200); 
-  
+  OpenBCI_Radio.ledFeedBackForPassThru();
 }
